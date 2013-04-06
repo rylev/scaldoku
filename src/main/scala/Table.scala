76a6fec4d1f474d9bc7b3bfbfd4883c9805ce021
Ryan Levick <ryan@6wunderkinder.com>
2013-04-06 19:36:34 +0200

    Validate rows, son

--- src/main/scala/Table.scala
+++ src/main/scala/Table.scala
@@ -1,16 +1,17 @@
 package com.sudoku
 
 class Table {
-  var board = 1 to 9 map { _ => 1 to 9 map { _ => -1 } }
+  // TODO: at some point we should define something smarter than 0 for empty.
+  var board = 1 to 9 map { _ => 1 to 9 map { _ => 0 } }
 
   def height = board.first.length
   def width = board.length
 
   def setValue(x: Integer, y: Integer, value: Integer): Boolean ={
     (x, y, value) match {
-      case (a, b, c) if (0 until 8 contains a) &&
-                        (0 until 8 contains b) &&
-                        (1 until 9 contains c) => true
+      case (a, b, c) if (0 to 8 contains a) &&
+                        (0 to 8 contains b) &&
+                        (0 to 9 contains c) => true
       case _ => return false
     }
 
@@ -25,7 +26,19 @@ class Table {
     true
   }
 
-  def getValue(x: Integer, y: Integer): Integer = {
+  def getValue(x: Integer, y: Integer) : Integer = {
     board(x)(y)
   }
+
+  def completeRow_?(rowNumber: Integer) : Boolean = {
+    val rowSet = row(rowNumber).toSet
+    rowSet.size == 9 && rowSet.min == 1 && rowSet.max == 9
+  }
+
+  def validRow_?(rowNumber: Integer) : Boolean = {
+    val cleanRow = row(rowNumber).filter { value => value != 0 }
+    cleanRow.toSet.size == cleanRow.size
+  }
+
+  def row(number: Integer) = board.map { column => column(number) }
 }
--- src/test/scala/TableTest.scala
+++ src/test/scala/TableTest.scala
@@ -18,4 +18,31 @@ class TableTest extends FunSuite {
     expect(true) { table.setValue(x = 5, y = 6, value = 5) }
     expect(5) { table.getValue(x = 5, y = 6) }
   }
+
+  test("validates complete rows") {
+    val table = new Table()
+    0 to 8 foreach { num => table.setValue(x = num, y = 0, value = num+1) }
+    expect(true) { table.completeRow_?(0) }
+  }
+
+  test("validates incomplete rows") {
+    val table = new Table()
+    0 to 8 foreach { num => table.setValue(x = num, y = 0, value = 5) }
+    expect(false) { table.completeRow_?(0) }
+  }
+
+  test("validates valid rows") {
+    val table = new Table()
+    0 to 8 foreach { num => table.setValue(x = num, y = 0, value = num+1) }
+    table.setValue(x = 3, y = 0, value = 0)
+    expect(true) { table.validRow_?(0) }
+  }
+
+  test("validates invalid rows") {
+    val table = new Table()
+    0 to 8 foreach { num => table.setValue(x = num, y = 0, value = 5) }
+    table.setValue(x = 3, y = 0, value = 0)
+    table.setValue(x = 2, y = 0, value = 4)
+    expect(false) { table.validRow_?(0) }
+  }
 }
