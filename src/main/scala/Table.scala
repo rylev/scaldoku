00db8c7e260e3db25cf9a75ae4fdea489200fe84
Josep M. Bach <josep.m.bach@gmail.com>
2013-04-20 18:37:02 +0200

    Extract Row in its own class

--- src/main/scala/Table.scala
+++ src/main/scala/Table.scala
@@ -9,43 +9,55 @@ class EmptyCell extends Cell {
   val value = null
 }
 
+class Row(val boardContents: Seq[Seq[Cell]], val index: Integer) {
+  def complete_? : Boolean = {
+    val uniqueRow = row(index).distinct
+    uniqueRow.size == 9 && uniqueRow.minBy { _.value }.value == 1 && uniqueRow.maxBy { _.value }.value == 9
+  }
+
+  def valid_? : Boolean = {
+    val cleanRowValues = row(index).filter { _.value != 0 }.map { _.value }
+    cleanRowValues.distinct.size == cleanRowValues.size
+  }
+
+  private def row(number: Integer): Seq[Cell] = boardContents.map { column => column(number) }
+
+  private def column(number: Integer): Seq[Cell] = boardContents(number)
+
+}
+
 class FilledCell(val value: Integer) extends Cell
 
-class Table(board: Seq[Seq[Cell]]) {
+class Table(contents: Seq[Seq[Cell]]) {
   def this() = this(1 to 9 map { _ => 1 to 9 map { _ => new EmptyCell } })
 
-  val height = board(0).length
-  val width = board.length
+  val height = contents(0).length
+  val width = contents.length
+  val rows = 0 to 8 map { new Row(contents, _) }
 
   def setValue(x: Integer, y: Integer, value: Integer): Table = {
     (x, y, value) match {
       case (a, b, c) if (0 to 8 contains a) &&
                         (0 to 8 contains b) &&
                         (0 to 9 contains c) => true
-      case _ => return new Table(board)
+      case _ => return new Table(contents)
     }
 
-    val newBoard = board.indices map { colIdx =>
-      board(colIdx).indices map { rowIdx =>
+    val newContents = contents.indices map { colIdx =>
+      contents(colIdx).indices map { rowIdx =>
         if (colIdx == x && rowIdx == y) new FilledCell(value)
-        else board(colIdx)(rowIdx) } }
+        else contents(colIdx)(rowIdx) } }
 
-    new Table(newBoard)
+    new Table(newContents)
   }
 
   def getValue(x: Integer, y: Integer): Integer = {
-    board(x)(y).value
+    contents(x)(y).value
   }
 
-  def completeRow_?(rowNumber: Integer) : Boolean = {
-    val uniqueRow = row(rowNumber).distinct
-    uniqueRow.size == 9 && uniqueRow.minBy { _.value }.value == 1 && uniqueRow.maxBy { _.value }.value == 9
-  }
+   def completeRow_?(rowNumber: Integer) : Boolean = rows(rowNumber) complete_?
 
-  def validRow_?(rowNumber: Integer) : Boolean = {
-    val cleanRowValues = row(rowNumber).filter { _.value != 0 }.map { _.value }
-    cleanRowValues.distinct.size == cleanRowValues.size
-  }
+   def validRow_?(rowNumber: Integer) : Boolean = rows(rowNumber) valid_?
 
   def completeColumn_?(columnNumber: Integer) : Boolean = {
     val uniqueColumn = column(columnNumber).distinct
@@ -53,14 +65,12 @@ class Table(board: Seq[Seq[Cell]]) {
   }
 
   def validColumn_?(columnNumber: Integer) : Boolean = {
-    //println(columnNumber)
     val cleanColumnValues = column(columnNumber).filter { _.value != 0 }.map { _.value }
-    //println(cleanColumnValues)
     cleanColumnValues.distinct.size == cleanColumnValues.size
   }
 
-  def printBoard() = {
-    board.zip(board.indices).map { columnAndIndex =>
+  def printContents() = {
+    contents.zip(contents.indices).map { columnAndIndex =>
       val column = columnAndIndex._1
       column.zip(column.indices).map { numAndIndex =>
         print(numAndIndex._1)
@@ -78,8 +88,8 @@ class Table(board: Seq[Seq[Cell]]) {
     }
   }
 
-  def row(number: Integer): Seq[Cell] = board.map { column => column(number) }
-  def column(number: Integer): Seq[Cell] = board(number)
+  def row(number: Integer): Seq[Cell] = contents.map { column => column(number) }
+  def column(number: Integer): Seq[Cell] = contents(number)
   def square(number: Integer) = {
     val xOffset = (number / 3) * 3
     val yOffset = (number % 3) * 3
